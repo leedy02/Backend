00555217cf36abf6adcfc0883ec345dcb5c425b6
leedy02 <flelrhddl@gmail.com>
2023-08-13 19:37:59 +0900

    first

---
+++ src/main/java/com/mecenatpolis/mcnp/McnpApplication.java
@@ -0,0 +1,13 @@
+package com.mecenatpolis.mcnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McnpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mecenatpolis/mcnp/McnpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mecenatpolis.mcnp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McnpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
